f85478fe9006d0bc24404a4aaa59e63965dbf4cd
Ayrat Kharrasov <airat.kharrasov@gmail.com>
2022-06-13 12:19:26 +0300

    Create simple interface for client

---
+++ cloud-app/src/main/java/com/geekbrains/cloud/june/cloudapp/HelloController.java
@@ -0,0 +1,13 @@
+package com.geekbrains.cloud.june.cloudapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class HelloController {
+
+    @FXML
+    ListView clientView;
+
+    @FXML
+    ListView serverView;
+}
